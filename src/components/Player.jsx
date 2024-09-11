88ffce9242e1039f0f92d2db36e201ba22cc2e7b
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-09-11 18:30:36 -0300

    feature: edit players names

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
+
 export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let usernameDisplay = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    usernameDisplay = (
+      <input type="text" value={playerName} onChange={handleChange} required />
+    );
+  }
+
   return (
     <li>
       <span className="player">
-        {initialName}
+        {usernameDisplay}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Save</button>
+      <button onClick={handleClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
