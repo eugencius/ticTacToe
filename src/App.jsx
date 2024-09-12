958faa800d80e2beeb2f4b8c10998a5431215ecb
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-09-12 10:18:01 -0300

    feature: display logs below gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
+import Log from './components/Log.jsx';
 
 function App() {
   const [activePlayer, setActivePlayer] = useState('X');
@@ -42,7 +43,7 @@ function App() {
 
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
