3faaf365f728c128c4887b6e5e3698811308f186
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-09-12 09:12:46 -0300

    feature: display player symbol and change players on click

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard({ activePlayerSymbol, onSelectSquare }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedGameBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedGameBoard;
+    });
+
+    onSelectSquare();
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => {
+                    handleSelectSquare(rowIndex, colIndex);
+                  }}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
